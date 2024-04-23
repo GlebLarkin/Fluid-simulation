2029a91794a3a3061827a50e4498636940f8e003
Gleb <larkin.gs@phystech.edu>
2024-04-23 15:03:01 +0300

    right interraction func

---
+++ build/CMakeFiles/MyProj.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyProj.dir/src/main.cpp.o"
+  "CMakeFiles/MyProj.dir/src/main.cpp.o.d"
+  "MyProj"
+  "MyProj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyProj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
